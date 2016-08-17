d88ab8f4a1afaae87cd4aac978e724fcac7ffa0f
code-waver <729634515@qq.com>
2016-08-17 16:17:43 +0800

    for循环

--- app/src/main/java/com/google/guo/myapplication/MainActivity.java
+++ app/src/main/java/com/google/guo/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.google.guo.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -22,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<40;i++){
             String s=">>>"+i;
             data.add(s);
+        }
+        for (int i = 0; i <10 ; i++) {
+            for(int j=0;j<20;j++){
+                Log.d("print", "onCreate: "+i);
+                Log.d("print", "onCreate: "+j);
+            }
+
         }
 
     }
