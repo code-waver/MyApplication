cd1b1960b9c0f29cc0e0aaaef9fb4fe252966d23
code-waver <729634515@qq.com>
2016-08-17 17:14:05 +0800

    version1

--- app/src/main/java/com/google/guo/myapplication/MainActivity.java
+++ app/src/main/java/com/google/guo/myapplication/MainActivity.java
@@ -2,14 +2,29 @@ package com.google.guo.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    List<String> data;
+    Myadapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //哈哈
+        lv= (ListView) findViewById(R.id.lv);
+        data=new ArrayList<>();
+        for(int i=0;i<40;i++){
+            String s=">>>"+i;
+            data.add(s);
+        }
+        adapter=new Myadapter(this,data);
+        lv.setAdapter(adapter);
+
     }
 }
