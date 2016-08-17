d1b1131919f4ddcf8dc03744a56cd82f19042bd4
code-waver <729634515@qq.com>
2016-08-17 16:39:58 +0800

    intent

--- app/src/main/java/com/google/guo/myapplication/MainActivity.java
+++ app/src/main/java/com/google/guo/myapplication/MainActivity.java
@@ -1,45 +1,15 @@
 package com.google.guo.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView lv;
-    List<String> data;
-    Myadapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv= (ListView) findViewById(R.id.lv);
-        data=new ArrayList<>();
-        for(int i=0;i<40;i++){
-            String s=">>>"+i;
-            data.add(s);
-        }
-        for (int i = 0; i <10 ; i++) {
-            for(int j=0;j<20;j++){
-                Log.d("print", "onCreate: "+i);
-                Log.d("print", "onCreate: "+j);
-            }
 
-        }
-
-        Intent intent1=new Intent();
-        startActivity(intent1);
-        Intent intent2=new Intent();
-        startActivity(intent2);
-        Intent intent3=new Intent();
-        startActivity(intent3);
-        Intent intent4=new Intent();
-        startActivity(intent4);
     }
 }
